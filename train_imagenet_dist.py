13a46647b8a77eedc76f9ad688856f60d4f5b46c
Wuyang Chen <chenwydj@gmail.com>
2020-11-12 21:42:26 -0600

    add thop

--- train_imagenet_dist.py
+++ train_imagenet_dist.py
@@ -166,6 +166,7 @@ def main_worker(gpu, ngpus_per_node, args):
     logging.info(genotype)
     print('--------------------------')
     model = Network(args.init_channels, CLASSES, args.layers, args.auxiliary, genotype)
+    model.drop_path_prob = 0
     macs, params = profile(model, inputs=(torch.randn(1, 3, 224, 224), ))
     logging.info("param = %f, flops = %f", params, macs)
 
@@ -206,7 +207,6 @@ def main_worker(gpu, ngpus_per_node, args):
     # else:
     #     model = model.cuda()
     logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
-    macs, params = profile(model, inputs=(torch.randn(1, 3, 224, 224), ))
 
     criterion = nn.CrossEntropyLoss()
     criterion = criterion.cuda()
