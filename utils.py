f7b129e5858fc22135b8f0d48d135c11f1ad3b17
Wuyang Chen <chenwydj@gmail.com>
2022-01-16 22:15:34 +0000

    bug fixed

--- utils.py
+++ utils.py
@@ -31,6 +31,7 @@ def accuracy(output, target, topk=(1,)):
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
+    correct = correct.contiguous()
 
     res = []
     for k in topk:
